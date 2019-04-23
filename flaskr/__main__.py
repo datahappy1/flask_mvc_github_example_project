6bd3798e31565372162063536f8cc0ccfc19a9e4
Pprud <pavel.prudky@accolade.com>
2019-04-24 00:04:41 +0200

    extension fix

--- flaskr/__main__.py
+++ flaskr/__main__.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flaskr.lib import gh, settings
+
+# settings
+temp_file_path = '../flaskr/files/temp/'
 
 # github related variables
 token = os.environ['token']
 # branch_name and local_file_path is temporary for testing
 repo = settings.repo
 branch_name = "dev"
-local_file_path = os.getcwd().rstrip('lib') + os.path.join('\\files', 'temp', 'test_' + branch_name + '.json')
+local_file_path = os.getcwd().rstrip('lib') + os.path.join('files', 'temp', 'test_' + branch_name + '.json')
 
 # init the github class
 obj = gh.GitHubClass(init_token=token, init_repo=repo, init_branch_name=branch_name,
@@ -15,6 +18,7 @@ obj = gh.GitHubClass(init_token=token, init_repo=repo, init_branch_name=branch_n
 
 # flask app starts here
 app = Flask(__name__)
+app.secret_key = 'abcd1234'
 
 # https://www.tutorialspoint.com/flask/flask_templates.htm
 # http://flask.pocoo.org/docs/1.0/tutorial/layout/
@@ -34,13 +38,15 @@ def config():
     b_list = gh.GitHubClass.list_all_branches(obj)
     f_list = gh.GitHubClass.list_all_files(obj)
 
+    temp_files = [f for f in os.listdir(temp_file_path) if os.path.isfile(os.path.join(temp_file_path, f))]
+
     return render_template('config.html', gh_session_id=gh_session_id,
-                           b_list=b_list, curr_branch=branch_name, f_list=f_list)
+                           b_list=b_list, curr_branch=branch_name, f_list=f_list,
+                           temp_f_list = temp_files)
 
 # https://www.tutorialspoint.com/flask/flask_file_uploading.htm
 # https://www.roytuts.com/python-flask-file-upload-example/
 # https://stackoverflow.com/questions/42424853/saving-upload-in-flask-only-saves-to-project-root
-# TODO /upload like a extension of the config template
 
 
 @app.route('/upload')
@@ -52,9 +58,24 @@ def upload():
 def uploader():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.abspath(os.path.dirname(__file__))+'', f.filename)
-
+        f.save(temp_file_path + f.filename)
+        flash(f.filename + ' was uploaded!')
         return redirect('/config')
+
+
+@app.route('/temp_files_merge', methods=['GET', 'POST'])
+def temp_files_merge():
+    outfilename = 'merged.csv'
+    temp_files = [f for f in os.listdir(temp_file_path) if os.path.isfile(os.path.join(temp_file_path, f))]
+
+    with open(outfilename, 'w') as outfile:
+        for fname in temp_files:
+            with open(temp_file_path + fname) as infile:
+                outfile.write(infile.read())
+
+            flash(fname + ' merged into ' + outfilename + '!')
+
+    return redirect('/config')
 
 
 @app.route('/workflow')
