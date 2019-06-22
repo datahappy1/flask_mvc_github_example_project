e040ed21b1f4a21a04eab94d5187af1a9cddd688
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-06-22 22:27:57 +0200

    init mvc structure

--- flaskr/__main__.py
+++ flaskr/__main__.py
@@ -1,15 +1,15 @@
 import os
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flaskr.lib import gh, settings
 
 # github related variables
 token = os.environ['token']
 # branch_name  is temporary for testing
 repo = settings.repo
-branch_name = settings.initial_branch_name
+init_branch_name = settings.initial_branch_name
 
 # init the github class
-obj = gh.GitHubClass(init_token=token, init_repo=repo, init_branch_name=branch_name)
+obj = gh.GitHubClass(init_token=token, init_repo=repo, init_branch_name=init_branch_name)
 
 # flask app starts here
 app = Flask(__name__)
@@ -18,17 +18,21 @@ app.secret_key = 'abcd1234'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           template_current_branch=init_branch_name)
 
 
-@app.route('/gh_files_manager')
-def gh_files_manager():
+@app.route('/views/gh_files_manager/branch/<branch_name>', methods=['GET', 'POST'])
+def gh_files_manager(branch_name):
     gh_session_id = gh.GitHubClass.get_session_id(obj)
     branch_list = gh.GitHubClass.list_all_branches(obj)
     files_list = gh.GitHubClass.list_all_files(obj)
 
-    return render_template('/views/gh_files_manager.html', gh_session_id=gh_session_id,
-                           b_list=branch_list, curr_branch=branch_name, f_list=files_list)
+    return render_template('views/gh_files_manager.html',
+                           gh_session_id=gh_session_id,
+                           template_branch_list=branch_list,
+                           template_current_branch=branch_name,
+                           template_file_list=files_list)
 
 
 @app.route('/upload')
