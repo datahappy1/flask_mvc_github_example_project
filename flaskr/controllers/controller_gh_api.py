37a020b59582e334e4b91b58c11d1000795122cc
Pprud <pavel.prudky@accolade.com>
2019-07-02 13:45:57 +0200

    features

--- flaskr/controllers/controller_gh_api.py
+++ flaskr/controllers/controller_gh_api.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, jsonify, request
 from flaskr.lib import global_variables, settings
 from flaskr.models import model_gh
-from flaskr.controllers import utils
+from flaskr.controllers import functions
 from werkzeug.utils import secure_filename
 
 
@@ -13,13 +13,14 @@ controller_gh_api = Blueprint('controller_gh_api', __name__)
 @controller_gh_api.route('/api/gh_branches_manager/', methods=['GET'])
 def api_gh_branches_manager():
     if request.method == "GET":
-        gh_session_id = utils.session_getter()[0]
-        branch_list = utils.branch_lister()
+        gh_session_id = functions.session_getter()[0]
+        branch_list = functions.branch_lister()
         response = jsonify({
             'gh_session_id': gh_session_id,
             'branches': branch_list,
             'repository': settings.repo,
-            'status': 'OK', #httplib.OK,
+            'method': request.method,
+            'status': 'OK',
             'mimetype': 'application/json'
         })
         response.status_code = 200
@@ -28,14 +29,14 @@ def api_gh_branches_manager():
 
 # curl -X POST http://127.0.0.1:5000/api/branch/test2?branch_name_src=test
 # curl -X DELETE http://127.0.0.1:5000/api/branch/test/
-@controller_gh_api.route('/api/branch/<branch_name>', methods=['POST', 'DELETE'])
+@controller_gh_api.route('/api/branch/<branch_name>/', methods=['POST', 'DELETE'])
 def api_branch(branch_name):
     if request.method == 'POST':
         args = request.args
         branch_name_src = args['branch_name_src']
         branch_name_tgt = branch_name
         branch_name_tgt = str(branch_name_tgt).replace(' ', '')
-        gh_session_id = utils.session_getter()[0]
+        gh_session_id = functions.session_getter()[0]
         model_gh.Branch.create_branch(global_variables.obj,
                                       source_branch=branch_name_src,
                                       target_branch=branch_name_tgt)
@@ -44,13 +45,15 @@ def api_branch(branch_name):
             'repository': settings.repo,
             'branch_tgt': branch_name_tgt,
             'branch_src': branch_name_src,
-            'method': request.method
+            'method': request.method,
+            'status': 'OK',
+            'mimetype': 'application/json'
         })
         response.status_code = 201
         return response
 
     if request.method == 'DELETE':
-        gh_session_id = utils.session_getter()[0]
+        gh_session_id = functions.session_getter()[0]
         model_gh.Branch.delete_branch(global_variables.obj,
                                       branch_name=branch_name)
 
@@ -58,7 +61,9 @@ def api_branch(branch_name):
             'gh_session_id': gh_session_id,
             'repository': settings.repo,
             'branch_name': branch_name,
-            'method': request.method
+            'method': request.method,
+            'status': 'OK',
+            'mimetype': 'application/json'
         })
         response.status_code = 200
         return response
@@ -68,15 +73,18 @@ def api_branch(branch_name):
 @controller_gh_api.route('/api/gh_files_manager/branch/<branch_name>/', methods=['GET'])
 def api_gh_files_manager(branch_name):
     if request.method == "GET":
-        gh_session_id = utils.session_getter()[0]
-        branch_list = utils.branch_lister()
-        files_list = utils.file_lister(branch_name)
+        gh_session_id = functions.session_getter()[0]
+        branch_list = functions.branch_lister()
+        files_list = functions.file_lister(branch_name)
         response = jsonify({
             'gh_session_id': gh_session_id,
             'branches': branch_list,
             'repository': settings.repo,
             'current_branch': branch_name,
-            'files': files_list
+            'files': files_list,
+            'method': request.method,
+            'status': 'OK',
+            'mimetype': 'application/json'
         })
         response.status_code = 200
         return response
@@ -90,7 +98,7 @@ def api_file(branch_name, file_name):
     if request.method == 'POST':
         args = request.args
         message = args['commit_message']
-        gh_session_id = utils.session_getter()[0]
+        gh_session_id = functions.session_getter()[0]
         file_name = secure_filename(file_name.filename)
 
         temp_file_path = os.path.join(os.getcwd(), 'temp', file_name)
@@ -111,7 +119,9 @@ def api_file(branch_name, file_name):
             'repository': settings.repo,
             'current_branch': branch_name,
             'file': file_name,
-            'method': request.method
+            'method': request.method,
+            'status': 'OK',
+            'mimetype': 'application/json'
         })
         response.status_code = 201
         return response
@@ -119,7 +129,7 @@ def api_file(branch_name, file_name):
     if request.method == 'PUT':
         args = request.args
         message = args['commit_message']
-        gh_session_id = utils.session_getter()[0]
+        gh_session_id = functions.session_getter()[0]
 
         temp_file_path = os.path.join(os.getcwd(), 'temp', file_name)
         with open(temp_file_path, "wb") as temp_file_handler:
@@ -136,7 +146,9 @@ def api_file(branch_name, file_name):
             'repository': settings.repo,
             'current_branch': branch_name,
             'file': file_name,
-            'method': request.method
+            'method': request.method,
+            'status': 'OK',
+            'mimetype': 'application/json'
         })
         response.status_code = 201
         return response
@@ -144,7 +156,7 @@ def api_file(branch_name, file_name):
     if request.method == 'DELETE':
         args = request.args
         message = args['commit_message']
-        gh_session_id = utils.session_getter()[0]
+        gh_session_id = functions.session_getter()[0]
         model_gh.File.delete_file(global_variables.obj,
                                   gh_file_path=file_name,
                                   message=message,
@@ -154,7 +166,9 @@ def api_file(branch_name, file_name):
             'repository': settings.repo,
             'current_branch': branch_name,
             'file': file_name,
-            'method': request.method
+            'method': request.method,
+            'status': 'OK',
+            'mimetype': 'application/json'
         })
         response.status_code = 200
         return response
--- flaskr/tests/test_api.py
+++ flaskr/tests/test_api.py
@@ -4,18 +4,20 @@ test api endpoints
 import pytest
 import requests
 
+
 @pytest.mark.parametrize(
     "test_name, method, endpoint, expected_success_status_code",[
-    ('get branches', 'get', 'http://127.0.0.1:5000/api/gh_branches_manager/', 200),
-    ('create branch', 'post', 'http://127.0.0.1:5000/api/branch/requests_test?branch_name_src=master', 201),
-    ('get files', 'get', 'http://127.0.0.1:5000/api/gh_files_manager/branch/requests_test/', 200),
-    ('create file', 'post', 'http://127.0.0.1:5000/api/branch/requests_test/my-file.txt?commit_message="pytest"', 200),
-    ('edit file', 'put', 'http://127.0.0.1:5000/api/branch/master/my-file.txt?commit_message="pytest"', 200),
-    ('delete file', 'delete', 'http://127.0.0.1:5000/api/branch/requests_test/my-file.txt?commit_message="pytest"', 200),
-    ('delete branch', 'delete', 'http://127.0.0.1:5000/api/branch/requests_test/', 200),
-     ])
+        ('create branch', 'post', 'http://127.0.0.1:5000/api/branch/requests_test?branch_name_src=master', 201),
+        ('get branches', 'get', 'http://127.0.0.1:5000/api/gh_branches_manager/', 200),
+        ('create file', 'post', 'http://127.0.0.1:5000/api/branch/requests_test/my-file.txt?commit_message="pytest"', 200),
+        ('edit file', 'put', 'http://127.0.0.1:5000/api/branch/master/my-file.txt?commit_message="pytest"', 200),
+        ('get files', 'get', 'http://127.0.0.1:5000/api/gh_files_manager/branch/requests_test/', 200),
+        ('delete file', 'delete', 'http://127.0.0.1:5000/api/branch/requests_test/my-file.txt?commit_message="pytest"', 200),
+        ('delete branch', 'delete', 'http://127.0.0.1:5000/api/branch/requests_test/', 200),
+    ])
 def test_request(test_name, method, endpoint, expected_success_status_code):
     print(test_name)
+
     if method == "get":
         response = requests.get(endpoint)
     elif method == "post":
@@ -24,21 +26,5 @@ def test_request(test_name, method, endpoint, expected_success_status_code):
         response = requests.get(endpoint)
     elif method == "delete":
         response = requests.get(endpoint)
+
     assert response.status_code == expected_success_status_code
-
-
-import requests
-resp1 = requests.get('http://127.0.0.1:5000/api/gh_branches_manager/')
-print(resp1)
-resp5 = requests.post('http://127.0.0.1:5000/api/branch/requests_test?branch_name_src=master')
-print(resp5)
-resp2 = requests.get('http://127.0.0.1:5000/api/gh_files_manager/branch/requests_test/')
-print(resp2)
-resp6 = requests.post('http://127.0.0.1:5000/api/branch/requests_test/my-file.txt?commit_message="pytest"')
-print(resp6)
-resp7 = requests.put('http://127.0.0.1:5000/api/branch/master/my-file.txt?commit_message="pytest"')
-print(resp7)
-resp3 = requests.delete('http://127.0.0.1:5000/api/branch/requests_test/my-file.txt?commit_message="pytest"')
-print(resp3)
-resp4 = requests.delete('http://127.0.0.1:5000/api/branch/requests_test/')
-print(resp4)
